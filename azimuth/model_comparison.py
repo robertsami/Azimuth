440b833a409481b099fe7bf3c9db6809b4eb61de
robertsami <robertsami@users.noreply.github.com>
2016-03-02 15:23:27 -0800

    Allow percent_peptide floats and run assertions over whole input arguments

--- azimuth/model_comparison.py
+++ azimuth/model_comparison.py
@@ -494,16 +494,13 @@ def predict(seq, aa_cut=-1, percent_peptide=-1, model=None, model_file=None, pam
     # assert not (model is None and model_file is None), "you have to specify either a model or a model_file"
     assert isinstance(seq, (str, np.ndarray)), "Please ensure seq is a numpy array"
     if isinstance(seq, np.ndarray) and len(seq) > 0:
-        assert isinstance(seq[0], str) or isinstance(seq[0], unicode), "Please ensure input sequences are in string format, i.e. 'AGAG' rather than ['A' 'G' 'A' 'G'] or alternate representations"
+        assert all(isinstance(s, (str, unicode)) for s in seq), "Please ensure input sequences are in string format, i.e. 'AGAG' rather than ['A' 'G' 'A' 'G'] or alternate representations"
     assert isinstance(aa_cut, (int, long, np.ndarray)), "Please ensure aa_cut is a numpy array"
     if isinstance(aa_cut, np.ndarray) and len(aa_cut) > 0:
-        assert isinstance(aa_cut[0], (int, long))
+        assert all(isinstance(cut, (int, long)) for cut in aa_cut)
     assert isinstance(percent_peptide, (int, long, np.ndarray)), "Please ensure percent_peptide is a numpy array"
     if isinstance(percent_peptide, np.ndarray) and len(percent_peptide) > 0:
-        assert isinstance(percent_peptide[0], (int, long))
-        
-    
-    
+        assert all(isinstance(pp, (float, int, long)) for pp in percent_peptide)
     
     print aa_cut, percent_peptide
     if model_file is None:
